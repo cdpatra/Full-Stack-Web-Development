bdada66a7ed61584b688e3ff2c60bc646365599a
cdpatra <cdpatra03@gmail.com>
2024-04-13 01:17:10 +0530

    completed functions with objects in JS

--- JavaScript/03_Basics/01_functions.js
+++ JavaScript/03_Basics/01_functions.js
@@ -51,5 +51,52 @@ console.log(loginUserMessage()); //output-> Please enter a username;    undefine
 function loginUserMessage2(username = "default user") {
   return `${username} just logged in`;
 }
-console.log(loginUserMessage2("cdpatra"));  //output-> cdpatra just logged in
-console.log(loginUserMessage2());   //output-> default user just logged in
+console.log(loginUserMessage2("cdpatra")); //output-> cdpatra just logged in
+console.log(loginUserMessage2()); //output-> default user just logged in
+
+// HANDELING MULTIPLE ARGUMENTS
+function calculateCartPrice(num1) {
+  return num1;
+}
+console.log(calculateCartPrice(200)); //output-> 200
+console.log(calculateCartPrice(200, 300, 400)); //output-> 200
+console.log();
+
+function calculateCartPrice2(...num1 /** rest operator */) {
+  return num1;
+}
+console.log(calculateCartPrice2(200, 300, 400)); //output-> [ 200, 300, 400 ] RETURN ARRAY
+console.log();
+
+function calculateCartPrice3(val1, val2, ...num1) {
+  return num1;
+}
+console.log(calculateCartPrice3(200, 300, 400, 2000)); //output-> [ 400, 2000 ]
+console.log();
+
+// FUNCTIONS WITH OBJECTS
+const user = {
+  unserName: "cdpatra",
+  price: 199,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+handleObject(user); //output-> Username is undefined and price is 199
+handleObject({
+  username: "sam",
+  prices: 399, //here the key name is prices not price so function will return undefined
+});
+// output-> Username is sam and price is undefined
+
+// FUNCTIONS WITH ARRAY
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArrray) {
+  return getArrray[1];
+}
+console.log(returnSecondValue(myNewArray)); //output-> 400
+console.log(returnSecondValue([300, 499, 100, 200])); //output-> 499
