fb478d5ba077eb201db002c33ec7915e81d4f8e9
cdpatra <cdpatra03@gmail.com>
2024-04-18 22:29:21 +0530

    completed Objects in JS and Visualize variable address in JS

--- JavaScript/13-Visualize-Variable-Address-in-JavaScript/script.js
+++ JavaScript/13-Visualize-Variable-Address-in-JavaScript/script.js
@@ -1,6 +1,18 @@
-const firstName = "Chinam";
-const middleName = "Dibyadyuti";
-const lastName = "Patra";
-const age = 21
+const firstName1 = "Akash";
+const firstName2 = "Akash";
+const firstName3 = "Aka" + "sh"; // address of firstName3 is not equal to firstName1 and firstName2 (refer the memory diagram to understand)
+console.log(firstName1 === firstName2); //output-> true
+console.log(firstName1 === firstName3); //output-> true
+/** IN PRIMITIVE DATA TYPES, COMPARISON OPERATORS COMPARE VARIABLE NOT ON THE BASIS OF THEIR ADDRESSES, RATHER ON THE BASIS OF THEIR VALUES, IF 2 VARIABLES HAVE DIFFERENT ADDRESS BUT HAVE SAVE VALUE THEN COMPARISON OPERATOR RETURN TRUE, BUT IN CASE OF NON-PRIMITIVE DATA TYPES, IF ADDRESS OF BOTH THE VARIABLES ARE DIFFERENT EVEN THOUGH THEIR VALUES ARE SAME, COMPARISON OPERATOR RETURN FALSE */
+
+const myNum1 = 21;
+const myNum2 = 21;
+
+const isGraduate = false;
 const hasJob = false;
 const isLearning = true;
+
+const myNull = null;
+const myUndefined = undefined;
+
+const myEmptyString = "";
---
+++ JavaScript/14-Objects-in-JavaScript/script.js
@@ -0,0 +1,71 @@
+var user = {}; //EMPTY OBJECT
+
+const user_1 = {
+  firstName: "Akash",
+  lastName: "Singh",
+  age: 26,
+  education: 12,
+  city: "Bangalore",
+};
+const user_2 = {
+  firstName: "Akash",
+  lastName: "Singh",
+  age: 26,
+  education: 12,
+  city: "Bangalore",
+};
+
+/** user1 and user2 have save values but their address are different, therefore non-primitive data types are reference type */
+console.log(user_1 === user_2); //output-> false
+
+const myName = "cdpatra"; //I created myName variable so that I can easily search my context on memory dev tools
+
+const username1 =
+  ""; /** here, both the strings will have same address, as equal values string are assigned to both the variables */
+const username2 = "";
+
+const user1 =
+  {}; /** here, even though both the objects are same, but they are reference type i.e. non-primitive data types therefore both will have different address   */
+const user2 = {};
+console.log(user1 === user2); //output-> false
+console.log(user1 == user2); //output-> false
+
+const myObject = {
+  firstName: "Adarsh", //INTERNALLY KEY IS CONSIDERED AS STRING
+  "last-name": "Singh",
+  cdpatra: "Developer",
+  address: {
+    city: "Bangalore",
+    pinCode: "876876",
+    state: "Karnataka",
+    moreDetails: {
+      population: 3142341342,
+      area: "312 sq km",
+    },
+  },
+};
+/** <| WHEN WE CREATE OBJECT WITHIN OBJECT, THEN A NEW OBJECT IS CREATED AT NEW ADDRESS AND THAT OBJECT'S REFERENCE IS GIVEN TO THE KEY OF THE PARENT OBJECT |> */
+
+// <<======< <| WAYS OF ACCESSING VALUES USING KEYS IN OBJECTS|> >=====>>
+console.log(myObject.firstName); //output-> Adarsh
+console.log(myObject["last-name"]); //output-> singh
+console.log(myObject[myName]); //output-> Developer
+console.log(myObject["first" + "Name"]); //output-> Adarsh
+
+// <<======< <| INSERTING NEW KEY VALUE PAIR IN OBJECT |> >=====>>
+console.log(myObject.age); //output-> undefined; as there is no key present in myObject named age;
+
+myObject.age = 21; //IN THIS WAY WE CAN ADD A NEW KEY VALUE PAIR IN OBJECT
+console.log(myObject["age"]); //output-> 21
+console.log(myObject); //output-> {firstName: 'Adarsh', last-name: 'Singh', cdpatra: 'Developer', age: 21}
+
+myObject["is-student"] = "true"; // THIS IS ANOTHER WAY TO ADD KEY VALUE PAIR IN OBJECT
+console.log(myObject["is-student"]); //output-> true
+
+// <<======< <| UPDATING KEY AND VALUE PAIRS IN OBJECT |> >=====>>
+myObject.cdpatra = "Software Engineer";
+console.log(myObject["cdpatra"]); //output-> Software Engineer
+
+// <<======< <| ACCESSING VALUES FROM OBJECT WITHIN OBJECT|> >=====>>
+console.log(myObject.address.city); //Bangalore;
+console.log(myObject["address"]["pinCode"]); //output-> 876876
