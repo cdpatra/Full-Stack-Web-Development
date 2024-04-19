56e4e6ff78fd4b1214ff7245886bb500e7e483fd
cdpatra <cdpatra03@gmail.com>
2024-04-20 00:51:48 +0530

    completed do while loop in JS

--- JavaScript/04-Variables-in-JavaScript/script.js
+++ JavaScript/04-Variables-in-JavaScript/script.js
@@ -1,53 +1,44 @@
-/** USING LET KEYWORD */
+// !<<================<< <| USING LET KEYWORD |> >>======================>>
 let firstName = "Chinam";
 let middleName = "Dibyadyuti";
 let lastName = "Patra";
 let age = 21;
 let isHappy = true;
-// let firstName = "hello"; //output-> ERROR    REDECLARATION OF LET VARIABLE IS NOT ALLOWED
+// & let firstName = "hello"; //output-> ERROR    RE DECLARATION OF LET VARIABLE IS NOT ALLOWED
 
 let userIntro =
-  "Hi, my name is " +
-  firstName +
-  " " +
-  middleName +
-  " " +
-  lastName +
-  ". I am " +
-  age +
-  " " +
-  "years old.";
+  "Hi, my name is " + firstName + " " + middleName + " " + lastName + ". I am " + age + " " + "years old.";
 console.log(userIntro);
 
-let a; //JS implicity stores undefined if the variable is uninitialised
+let a; //~ JS implicitly stores undefined if the variable is uninitialized
 console.log(typeof a); //output-> undefined
 
 let b = null;
 console.log(typeof b); //output-> object
 
-/** USING CONST KEYWORD */
-// if we don't want to change the variable's value and data type once it is initialised we use const variable
+// ^USING CONST KEYWORD
+// & if we don't want to change the variable's value and data type once it is initialized we use const variable
 
 const hoursInDay = 24;
 // hoursInDay = 18;    //output-> ERROR
 
-// const anotherVariable;   //output-> ERROR   'const' declarations must be initialized.
+//>> const anotherVariable;   //output-> ERROR   'const' declarations must be initialized.
 
-/** USING VAR KEYWORD AS WELL AS DIFFERENCE BETWEEN VAR AND LET OR CONST*/
+// !<<===========<< <| USING VAR KEYWORD AS WELL AS DIFFERENCE BETWEEN VAR AND LET OR CONST |> >>================>>
 var varVariable = "hello";
-var varVariable = "world"; //output-> NO ERROR     REDECLARATION OF VAR VARIABLE IS ALLOWED
+var varVariable = "world"; //?output-> NO ERROR     RE DECLARATION OF VAR VARIABLE IS ALLOWED
 console.log(varVariable);
 {
   var anotherVarVariable = 100;
 }
-console.log(anotherVarVariable); //output-> 100;     VAR VARIABLE CAN BE ACCESSIBLE OUT OF THE SCOPE ALSO BUT LET AND CONST CANNOT
+console.log(anotherVarVariable); //?output-> 100;     VAR VARIABLE CAN BE ACCESSIBLE OUT OF THE SCOPE ALSO BUT LET AND CONST CANNOT
 
-/** 1. LET AND CONST ARE BLOCK SCOPED AND VAR IS FUNCTIONAL SCOPED */
+//&  1. LET AND CONST ARE BLOCK SCOPED AND VAR IS FUNCTIONAL SCOPED
 
-/** 2. IF WE CONSOLE.LOG THE VAR VARIABLE BEFORE ITS DECLARATION THEN IT IS NOT ERROR BUT WHEN WE CONSOLE.LOG LET AND CONST VARIABLE BEFORE THEIR DECLARATION THEN IT IS AN ERROR */
-console.log(varVariable2);  //output-> undefined
+// &/** 2. IF WE CONSOLE.LOG THE VAR VARIABLE BEFORE ITS DECLARATION THEN IT IS NOT ERROR BUT WHEN WE CONSOLE.LOG LET AND CONST VARIABLE BEFORE THEIR DECLARATION THEN IT IS AN ERROR */
+console.log(varVariable2); //~output-> undefined
 var varVariable2;
-// console.log(letVariable);   //output-> ERROR
+// console.log(letVariable);   //~output-> ERROR
 // let letVariable;
 
-/** The Temporal Dead Zone (TDZ) is a concept in JavaScript that relates to the hoisting of variables and the visibility of variables declared with let and const. In the TDZ, a variable exists but it cannot be accessed until it is declared. This prevents the variable from being used or accessed before a value is assigned to it. */
+// &/** The Temporal Dead Zone (TDZ) is a concept in JavaScript that relates to the hoisting of variables and the visibility of variables declared with let and const. In the TDZ, a variable exists but it cannot be accessed until it is declared. This prevents the variable from being used or accessed before a value is assigned to it. */
---
+++ JavaScript/23-Do-While-Loop-in-JavaScript/script.js
@@ -0,0 +1,7 @@
+// ! <<=============<< <| DO WHILE LOOP |> >>=====================>>
+// & DO WHILE RUNS AT LEAST ONE TIME IN THE START
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
