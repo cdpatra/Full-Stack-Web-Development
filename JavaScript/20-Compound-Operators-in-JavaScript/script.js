8238ca1d52a8ccb7a795d0ad39981ce3197b1e89
cdpatra <cdpatra03@gmail.com>
2024-04-19 23:25:16 +0530

    compeleted compound operators in JS

---
+++ JavaScript/20-Compound-Operators-in-JavaScript/script.js
@@ -0,0 +1,43 @@
+// ! <<=====================<< <| Compound(Combined Assignment) Operators |> >>===========================>>
+
+let num = 5;
+num += 5; // num = num + 5
+console.log(num); //output-> 10
+
+num = 5;
+num -= 5; //num = num - 5
+console.log(num); //output-> 0
+
+num = 5;
+num %= 5; // num = num % 5
+console.log(num); //output-> 0
+
+num = 5;
+num *= 5; //num = num * 5
+console.log(num); //output-> 25
+
+num = 5;
+num /= 5; //num = num / 5
+console.log(num); //output-> 1
+
+num = 5;
+num **= 5; //num = num ** 5
+console.log(num); //output-> 3125
+
+// ! <<=======================<< <| INCREMENT OPERATOR |> >>===========================>>
+num = 5;
+// ~ post increment
+console.log(num++); //output-> 5
+console.log(num); //output-> 6
+// ~ pre increment
+console.log(++num); //output-> 7
+console.log(num); //output-> 7
+
+// ! <<======================<< <| DECREMENT OPERATOR |> >>============================>>
+num = 5;
+// ~ post decrement
+console.log(num--); //output-> 5
+console.log(num); //output-> 4
+// ~ pre decrement
+console.log(--num); //output-> 3
+console.log(num); //output-> 3
