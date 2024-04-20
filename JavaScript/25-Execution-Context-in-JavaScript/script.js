f8f89b2cc62c99eb666496c85337090b103bd3ac
cdpatra <cdpatra03@gmail.com>
2024-04-20 11:59:09 +0530

    completed execution context in JS

---
+++ JavaScript/25-Execution-Context-in-JavaScript/script.js
@@ -0,0 +1,16 @@
+sayHi();
+
+const username = "Matilda";
+const userAge = 25;
+
+function sayHi() {
+  const a = 14;
+  const b = 12;
+  console.log(a + b);
+  console.log("Hi");
+  add(a, b);
+}
+
+function add(x, y) {
+  return x + y;
+}
