fbc9bbccf2b411dbf3858cac38fac09a98add716
cdpatra <cdpatra03@gmail.com>
2024-04-22 23:48:57 +0530

    learned DOM in JS

---
+++ JavaScript/39-DOM-in-JavaScript/script.js
@@ -0,0 +1,5 @@
+// ! <<===============<< DOCUMENT OBJECT MODEL >>=============>>
+// ^ document is itself an object
+console.log(typeof document); //output-> object
+console.dir(document.children[0].children[1].children);
+console.dir(document.body.children);
