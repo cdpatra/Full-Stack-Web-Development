3218e5720adb56f9fb0f75984f72370947b36765
cdpatra <cdpatra03@gmail.com>
2024-05-03 11:25:40 +0530

    learned some mouse events in JS

---
+++ JavaScript/40-DOM-Manipulation-in-JavaScript/12-Mouse-events-in-JavaScript/script.js
@@ -0,0 +1,52 @@
+// ! <<==============<< Mouse events in JavaScript >>===================>>
+const container = document.querySelector(`.container`);
+const newCardButton = document.querySelector(`.add-card`);
+let count = 1;
+
+// ! click event
+// newCardButton.addEventListener(`click`, () => {
+//    const newCard = document.createElement(`div`);
+//    newCard.classList.add(`card`);
+//    newCard.innerHTML = count++;
+//    container.append(newCard);
+// });
+
+// ! mousedown event
+// newCardButton.addEventListener(`mousedown`, () => {
+//    const newCard = document.createElement(`div`);
+//    newCard.classList.add(`card`);
+//    newCard.innerHTML = count++;
+//    container.append(newCard);
+// });
+
+// ! mouseup event
+// newCardButton.addEventListener(`mouseup`, () => {
+//    const newCard = document.createElement(`div`);
+//    newCard.classList.add(`card`);
+//    newCard.innerHTML = count++;
+//    container.append(newCard);
+// });
+
+// ! mouseenter event
+// newCardButton.addEventListener(`mouseenter`, () => {
+//    const newCard = document.createElement(`div`);
+//    newCard.classList.add(`card`);
+//    newCard.innerHTML = count++;
+//    container.append(newCard);
+// });
+
+// ! mouseleave event
+// newCardButton.addEventListener(`mouseleave`, () => {
+//    const newCard = document.createElement(`div`);
+//    newCard.classList.add(`card`);
+//    newCard.innerHTML = count++;
+//    container.append(newCard);
+// });
+
+// ! mousemove event
+// newCardButton.addEventListener(`mousemove`, () => {
+//    const newCard = document.createElement(`div`);
+//    newCard.classList.add(`card`);
+//    newCard.innerHTML = count++;
+//    container.append(newCard);
+// });
